6d963d77428092e23197611e5e0f601ea3389a53
Matthew Kim <kimm5294@gmail.com>
2017-05-10 16:45:00 -0400

    fix syntax error in render so the create renders the user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
 
 
-	def show
-		@user = User.find_by(id: params[:id])
-	end
+  def show
+    @user = User.find_by(id: params[:id])
+  end
 
-	def new
-		# @user = User.new
-	end
+  def new
+    # @user = User.new
+  end
 
 	def create
 		# render plain: user_params.inspect
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
-			render "users/@user.id"
-		else
-			@errors = @user.errors.full_messages
-			render 'new'
+			render "users/show"
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
 		end
 
 	end
