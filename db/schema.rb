3e045abe31fcfb5b01a1038af2cbf2717859372a
Matthew Kim <kimm5294@gmail.com>
2017-05-10 16:53:53 -0400

    work on show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510184941) do
+ActiveRecord::Schema.define(version: 20170510161826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,16 +49,6 @@ ActiveRecord::Schema.define(version: 20170510184941) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["seller_id"], name: "index_items_on_seller_id"
-  end
-
-  create_table "transactions", force: :cascade do |t|
-    t.bigint "buyer_id", null: false
-    t.bigint "item_id", null: false
-    t.integer "quantity", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["buyer_id"], name: "index_transactions_on_buyer_id"
-    t.index ["item_id"], name: "index_transactions_on_item_id"
   end
 
   create_table "users", force: :cascade do |t|
