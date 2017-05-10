0f3620b1022a29ad20148a7c4f7ea9adf9d95315
Slip <email": null>
2017-05-10 16:35:57 -0400

    working login and logout pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
-			redirect_to root_path
+			render "users/@user.id"
 		else
 			@errors = @user.errors.full_messages
 			render 'new'
