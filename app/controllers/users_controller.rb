1cdb9853ed85052b09f564c1347ff3734a661b9f
Bryan Mateer <bryanxmateer@gmail.com>
2017-05-10 17:18:30 -0400

    Fix indentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,22 +9,22 @@ class UsersController < ApplicationController
     # @user = User.new
   end
 
-	def create
-		# render plain: user_params.inspect
-		@user = User.new(user_params)
-		if @user.save
-			session[:user_id] = @user.id
-			render "users/show"
+  def create
+    # render plain: user_params.inspect
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render "users/show"
     else
       @errors = @user.errors.full_messages
       render 'new'
-		end
+    end
 
-	end
+  end
 
-	private
-	def user_params
-		params.require(:user).permit(:username, :email, :password, :password_confirmation)
-	end
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 
 end
