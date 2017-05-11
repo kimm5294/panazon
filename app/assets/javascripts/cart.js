486c20b409ac3c3a7fec0c123000c5c81795cf77
Bryan Mateer <email": null>
2017-05-11 18:13:36 -0400

    Start handling ajax for updating item quantity

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  
-});
+// $(document).ready(function() {
+//   $("#update-quantity").on("select", function(event) {
+//     event.preventDefault();
+//
+//     var url = $(this).parent().attr("action");
+//     var data = $(this).serialize();
+//
+//     $.ajax({
+//       type: "put",
+//       url: url,
+//       data: data
+//     })
+//   })
+// });
