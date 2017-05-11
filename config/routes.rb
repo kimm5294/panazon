31d44610c93b916114cf195204dc97b67e9dbc14
Gurkanver Brar <Brargurkanver@gmail.com>
2017-05-11 11:55:47 -0400

    added homepage controller and view, added layout for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'homepage#index'
+
 
   resources :categories, only: [:index, :show] do
     resources :items, except: [:index]
   end
-  
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   root 'sessions#new'
