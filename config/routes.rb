770224060805a895c199d3fbd777cf0c8f3c3853
Bryan Mateer <email": null>
2017-05-11 10:36:10 -0400

    Add show route for cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show] do
     resources :items, except: [:index]
   end
-  
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :carts, only: [:show]
   root 'sessions#new'
 
 end
