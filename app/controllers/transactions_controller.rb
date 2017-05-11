6639a70e9dbfb8277213d2564a1710e1c5eabfe0
Bryan Mateer <email": null>
2017-05-11 11:49:50 -0400

    Create routes for destroy and create

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,9 +1,18 @@
 class TransactionsController < ApplicationController
 
   def destroy
-    @user = current_user
     @purchase = Transaction.find_by(id: params[:id])
     @purchase.destroy
-    redirect_to "/users/#{@user.id}/cart"
+    redirect_to "/users/#{current_user.id}/cart"
   end
+
+  def create
+    Transaction.create!(transaction_params)
+    redirect_to "/users/#{current_user.id}/cart"
+  end
+
+  private
+    def transaction_params
+      params.require(:transaction).permit(:buyer_id, :item_id, :quantity)
+    end
 end
